47c16bb8f9247c485cd94be33a0f2ad7fa1958ce
nader <nader.al.aghbari1@gmail.com>
2020-03-27 10:21:08 +0100

    init projet

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+function ajouteChiffre(a)
+{
+    
+document.getElementById('resultat').innerHTML +=a
+}
+
+function machaine
+
+if (condition) {
+    
+}  (machain.index('+')  machain.index('-')  machain.index('/')  || machain.index('*') ) {} 
+else { doc.getElem.inner}
+
+machain.indexOf('+') != -1
+
+
